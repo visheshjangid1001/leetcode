3de63ba7cb125407af4d89d43d586d646c0fd317
Vishesh Jangid <visheshjangid1234@gmail.com>
2026-03-23 21:33:40 +0530

    Linked list

---
+++ LinkedList/SLL.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+        
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+    ## append val in linked list
+    def append(self,val):
+        new_node = Node(val)
+        if self.head == None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+            
+    ## Traverse 
+    def traverse(self):
+        if self.head == None:
+            print("sll is empty")
+        else:
+            current = self.head
+            while current is not None:
+                print(current.val,end=" ")
+                current = current.next
+                
+                
+sll = SinglyLinkedList()
+sll.append(2)
+sll.append(6)
+sll.append(10)
+sll.append(9)
+sll.append(8)
+sll.traverse()
